094dffbb5c02b374fdee7a3052e5801715b4e9d4
unknown <deshwaltushar933@gamil.com>
2026-09-26 11:57:55 +0530

    add button

---
+++ button.jsx
@@ -0,0 +1,10 @@
+export default function
+button({action}){
+    return(
+        <div>
+            <button onClick={action}>
+                generate
+            </button>
+        </div>
+    )
+}
